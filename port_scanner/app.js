8c4abf73defd11e7098488b1881c18c5f5602c59
Eric-Lingren <44101756+Eric-Lingren@users.noreply.github.com>
2019-03-22 01:50:18 -0600

    working on port scanner

--- port_scanner/app.js
+++ port_scanner/app.js
@@ -1,34 +1,39 @@
-const net = require('net');
+// const net = require('net');
 let host = '192.168.0.30'
-console.log(net.Socket)
+// console.log(net.Socket)
 
 let startingPort = 1;
 let endingPort = 10000;
 let timeout = 2000;
-let output = document.getElementById('output')
+// let output = document.getElementById('output')
 
-while (startingPort <= endingPort) {
-    let port = startingPort;
+// while (startingPort <= endingPort) {
+//     let port = startingPort;
     
-    (function(port) {
-        var socket = new net.Socket();
+//     (function(port) {
+//         var socket = new net.Socket();
         
-        socket.setTimeout(timeout, function() { socket.destroy(); });
-        socket.connect(port, host, function() {
-            console.log('OPEN: ' + port);
-            output.innerHTML = port
-        });
+//         socket.setTimeout(timeout, function() { socket.destroy(); });
+//         socket.connect(port, host, function() {
+//             console.log('OPEN: ' + port);
+//             // output.innerHTML = port
+//         });
 
-        socket.on('data', function(data) {
-            console.log(port + ': ' + data);
-            socket.destroy();
-        });
+//         socket.on('data', function(data) {
+//             console.log(port + ': ' + data);
+//             socket.destroy();
+//         });
         
-        socket.on('error', function(e) {
-            socket.destroy();
-        });
-    })(port);
+//         socket.on('error', function(e) {
+//             socket.destroy();
+//         });
+//     })(port);
     
-    startingPort++;
-}
+//     startingPort++;
+// }
+
+
+let myImage = document.getElementById('myImage')
+myImage.src = 'http://192.168.1/myimage'
+console.dir(myImage.src)
 
